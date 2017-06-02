089ae9b99b2bad48f20fbe6696e7a07b9f257097
Greger Olofsson <greger.olofsson@gmail.com>
2017-06-02 23:44:37 +0200

    v1.0

---
+++ src/CopyTray/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace CopyTray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.BalloonTipText = "Done.";
+            this.trayIcon.ContextMenuStrip = this.contextMenu;
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "trayIcon";
+            this.trayIcon.Visible = true;
+            this.trayIcon.Click += new System.EventHandler(this.trayIcon_Click);
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.contextMenu.Name = "contextMenuStrip1";
+            this.contextMenu.Size = new System.Drawing.Size(109, 36);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(105, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(108, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.OnExitMenuItemClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 44);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "CopyTray";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ src/CopyTray/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CopyTray
+{
+    public partial class Form1 : Form
+    {
+        private IDictionary<string, string> _files;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadFiles();
+            AddFilesToContextMenu();
+        }
+
+        //https://stackoverflow.com/questions/357076/best-way-to-hide-a-window-from-the-alt-tab-program-switcher
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var Params = base.CreateParams;
+                Params.ExStyle |= 0x80;
+                return Params;
+            }
+        }
+
+        private void LoadFiles()
+        {
+            _files = new Dictionary<string, string>();
+
+            var currentDirectoryInfo = new DirectoryInfo(Application.StartupPath);
+            var txtInfos = currentDirectoryInfo.EnumerateFiles("*.txt").ToList();
+            foreach (var txtInfo in txtInfos)
+            {
+                using (var fileStream = File.OpenText(txtInfo.FullName))
+                {
+                    _files.Add(txtInfo.Name, fileStream.ReadToEnd());
+                }
+            }
+        }
+
+        private void AddFilesToContextMenu()
+        {
+            var itemOffset = contextMenu.Items.Count;
+
+            foreach (var file in _files)
+            {
+                var i = contextMenu.Items.Count - itemOffset;
+                contextMenu.Items.Insert(i,new ToolStripMenuItem(file.Key,null,OnFileMenuItemClicked));
+            }
+        }
+
+        private void OnFileMenuItemClicked(object sender, EventArgs eventArgs)
+        {
+            var menuItem = (ToolStripMenuItem) sender;
+            Clipboard.SetText(_files[menuItem.Text]);
+        }
+
+        private void trayIcon_Click(object sender, EventArgs e)
+        {
+            var args = (MouseEventArgs)e;
+            if (args.Button != MouseButtons.Left) return;
+
+            Clipboard.SetText(_files.First().Value);
+        }
+
+        private void OnExitMenuItemClicked(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
